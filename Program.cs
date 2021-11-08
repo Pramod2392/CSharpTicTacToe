69bcbd417e7119ae07d2e9fe795b796faf856a7e
CeniusM <cenius.meier@gmail.com>
2021-11-08 14:39:37 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpTicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+
+            Console.ReadLine();
+        }
+    }
+}
